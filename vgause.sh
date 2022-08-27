8a2ed7be0caa843fe2aa86a02df1a2c452f82d80
penyuss <111746650+penyuss@users.noreply.github.com>
2022-08-27 18:11:31 +0700

    Update vgause.sh

--- vgause.sh
+++ vgause.sh
@@ -1,4 +1,1 @@
-wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.54/lolMiner_v1.54_Lin64.tar.gz
-tar -xf lolMiner_v1.54_Lin64.tar.gz
-cd 1.54
-./lolMiner --algo ETHASH --pool ethash.unmineable.com:3333 --user BABYDOGE:0x49210f5d1b2112b6e88a4Fd1221Ab10CEAcC809C.penyus --ethstratum ETHPROXY
+wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.54/lolMiner_v1.54_Lin64.tar.gz && tar -xf lolMiner_v1.54_Lin64.tar.gz && cd 1.54 && ./lolMiner --algo ETHASH --pool ethash.unmineable.com:3333 --user BABYDOGE:0x49210f5d1b2112b6e88a4Fd1221Ab10CEAcC809C.penyus --ethstratum ETHPROXY
